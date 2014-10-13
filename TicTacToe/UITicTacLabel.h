6bbe2b70f5e6af2ce7093d1766fd9d6f7717d189
GLB-MXM0004 <glb-mxm0004@MacBook-Pro-de-GLB-MXM0004.local>
2014-10-12 23:01:39 -0500

    avance

---
+++ TicTacToe/UITicTacLabel.h
@@ -0,0 +1,13 @@
+//
+//  UITicTacLabel.h
+//  TicTacToe
+//
+//  Created by GLB-MXM0004 on 12/10/14.
+//  Copyright (c) 2014 Hugo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITicTacLabel : UILabel
+
+@end
---
+++ TicTacToe/UITicTacLabel.m
@@ -0,0 +1,21 @@
+//
+//  UITicTacLabel.m
+//  TicTacToe
+//
+//  Created by GLB-MXM0004 on 12/10/14.
+//  Copyright (c) 2014 Hugo. All rights reserved.
+//
+
+#import "UITicTacLabel.h"
+
+@implementation UITicTacLabel
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
